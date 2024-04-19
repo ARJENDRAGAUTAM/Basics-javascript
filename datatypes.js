e2ea358fb09c4882cc3c3b693bc82f62fde43f4d
ARJENDRAGAUTAM <167452712+ARJENDRAGAUTAM@users.noreply.github.com>
2024-04-19 17:12:17 +0000

    config done

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+// ********** primitive datatypes*******
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => ture or false
+// null => standalone value 
+// undefined => 
+// symbol => unique
+// ********** non-primitive *************
+// object
+// console.log(typeof null); => object
--- variables.js
+++ variables.js
@@ -1,3 +1,13 @@
-let a;
-  a = "arjendra";
-  console.log(a);
+// ******************* const**********************
+
+// // const a = {
+// //   prop1 : 10,
+// //   prop2 : 9 
+// // }
+// // a.prop1 = 3 ;
+// // console.log(a); ************* output = { prop1: 3, prop2: 9}
+// a {
+//   b:10,
+//   prop2 :9
+// }
+// console.log(b);**************** output =  unexpected syntaxError
